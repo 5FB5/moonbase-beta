8302bd36f793431fd6888d2b122afcfc8d27c695
5FB5 <vecktor911@gmail.com>
2021-06-24 22:50:38 +0300

    [map] updated skybox, lighting

---
+++ code/MoonbaseBetaHud.cs
@@ -0,0 +1,17 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[Library]
+
+public partial class MoonbaseBetaHud : HudEntity<RootPanel>
+{
+	public MoonbaseBetaHud()
+	{
+		if ( !IsClient )
+			return;
+
+		RootPanel.AddChild<ChatBox>();
+	}
+}
+
+
--- code/MoonbaseBetaPlayer.cs
+++ code/MoonbaseBetaPlayer.cs
@@ -17,4 +17,19 @@ partial class MoonbasePlayer : Player
 
 		base.Respawn();
 	}
+
+	public override void Simulate( Client cl )
+	{
+		base.Simulate( cl );
+
+		if ( IsServer && Input.Pressed( InputButton.Attack1 ) )
+		{
+			var ragdoll = new ModelEntity();
+			ragdoll.SetModel( "models/citizen/citizen.vmdl" );
+			ragdoll.Position = EyePos + EyeRot.Forward * 40;
+			ragdoll.Rotation = Rotation.LookAt( Vector3.Random.Normal );
+			ragdoll.SetupPhysicsFromModel( PhysicsMotionType.Dynamic, false );
+			ragdoll.PhysicsGroup.Velocity = EyeRot.Forward * 1000;
+		}
+	}
 }
