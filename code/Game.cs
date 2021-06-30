7ee427b991ca3ecde12d863807c77685d7e28de2
5FB5 <vecktor911@gmail.com>
2021-06-30 22:17:53 +0300

    fixed crashing on loading

---
+++ code/Game.cs
@@ -0,0 +1,28 @@
+using Sandbox;
+
+public partial class MoonbaseGame : Sandbox.Game
+{
+	public MoonbaseGame()
+	{
+		if (IsServer)
+		{
+			ConsoleSystem.Run( "sv_gravity 132.8" );
+			Log.Info( "Server: sv_gravity 132.8" );
+
+			ConsoleSystem.Run( "playsound music_martian_cowboy" );
+			Log.Info( "Server: playsound music_martian_cowboy" );
+
+			new MoonbaseBetaExplorationHud();
+		}
+
+	}
+
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+		var player = new MoonbasePlayer();
+		cl.Pawn = player;
+
+		player.Respawn();
+	}
+}
---
+++ code/ui/MoonbaseBetaExplorationHud.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[Library]
+public partial class MoonbaseBetaExplorationHud : HudEntity<RootPanel>
+{
+	public MoonbaseBetaExplorationHud()
+	{
+		if ( !IsClient )
+			return;
+
+		RootPanel.StyleSheet.Load( "ui/MoonbaseBetaHud.scss" );
+
+		RootPanel.AddChild<NameTags>();
+		RootPanel.AddChild<CrosshairCanvas>();
+		RootPanel.AddChild<ChatBox>();
+		RootPanel.AddChild<VoiceList>();
+		RootPanel.AddChild<KillFeed>();
+		RootPanel.AddChild<Scoreboard<ScoreboardEntry>>();
+
+	}
+}
+
+
