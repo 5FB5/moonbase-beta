ad3ffbed8c583fec9fa8b416dcb6483606511d69
5FB5 <vecktor911@gmail.com>
2021-06-30 21:48:40 +0300

    added ui

--- code/Game.cs
+++ code/Game.cs
@@ -2,14 +2,23 @@ using Sandbox;
 
 public partial class MoonbaseGame : Sandbox.Game
 {
+	public MoonbaseGame()
+	{
+		if (IsServer)
+		{
+			ConsoleSystem.Run( "sv_gravity 132.8" );
+			ConsoleSystem.Run( "playsound music_martian_cowboy" );
+
+			new MoonbaseBetaExplorationHud();
+		}
+
+	}
+
 	public override void ClientJoined( Client cl )
 	{
 		base.ClientJoined( cl );
 		var player = new MoonbasePlayer();
 		cl.Pawn = player;
-
-		ConsoleSystem.Run( "sv_gravity 132.8" );
-		ConsoleSystem.Run( "playsound music_martian_cowboy" );
 
 		player.Respawn();
 	}
