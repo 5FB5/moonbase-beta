fcbbd41ee4940f8c1db4736a2b7aaa8ba9e7df81
5FB5 <vecktor911@gmail.com>
2021-07-02 14:36:06 +0300

    [code[ deleted playsound command

--- code/Game.cs
+++ code/Game.cs
@@ -1,8 +1,4 @@
 using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.IO;
-using System.Threading.Tasks;
 
 public partial class MoonbaseGame : Sandbox.Game
 {
@@ -17,13 +13,9 @@ public partial class MoonbaseGame : Sandbox.Game
 
 			ConsoleSystem.Run( "sv_gravity 132.8" );
 			Log.Info( "Server: sv_gravity 132.8" );
-
-			//ConsoleSystem.Run( "playsound music_martian_cowboy" );
-			//Log.Info( "Server: playsound music_martian_cowboy" );
 		}
 
 	}
-
 	public override void ClientJoined( Client cl )
 	{
 		base.ClientJoined( cl );
