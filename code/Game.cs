6d235b341cb4fed0425060ce9c650c323ea99c3c
5FB5 <vecktor911@gmail.com>
2021-07-02 14:21:45 +0300

    [code] enabled changing gravity with last sbox fix

--- code/Game.cs
+++ code/Game.cs
@@ -6,13 +6,13 @@ public partial class MoonbaseGame : Sandbox.Game
 	{
 		if (IsServer)
 		{
+			Log.Info( "[MoonbaseBeta] You've connected as HOST" );
+
 			ConsoleSystem.Run( "sv_gravity 132.8" );
 			Log.Info( "Server: sv_gravity 132.8" );
 
 			ConsoleSystem.Run( "playsound music_martian_cowboy" );
 			Log.Info( "Server: playsound music_martian_cowboy" );
-
-			new MoonbaseBetaExplorationHud();
 		}
 
 	}
@@ -20,9 +20,18 @@ public partial class MoonbaseGame : Sandbox.Game
 	public override void ClientJoined( Client cl )
 	{
 		base.ClientJoined( cl );
+
 		var player = new MoonbasePlayer();
 		cl.Pawn = player;
 
 		player.Respawn();
+	}
+
+	public override void Shutdown()
+	{
+		ConsoleSystem.Run( "sv_gravity 600" );
+		Log.Info( "Server (Shutdown): sv_gravity 600" );
+
+		base.Shutdown();
 	}
 }
